90494cc7972c1af095a31a280474e0724d3debbc
JamesOverclock <wirayut.temsao@gmail.com>
2019-01-04 09:53:23 +0700

    first commit

---
+++ app/src/main/java/com/l3ios/recyclerviewsearchfilter/model/Contact.java
@@ -0,0 +1,19 @@
+package com.l3ios.recyclerviewsearchfilter.model;
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
